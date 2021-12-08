a81c693ae8eb0253ff45b7267b34ef412dda42b4
gadlab <gadlab@ik.me>
2021-12-08 11:42:31 +0100

    Restructured pages components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
+import PageBody from './components/page';
+
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
-import { PageBody, ResponsiveAppBar } from './ui';
 
 export default function DisplayApp() {
   return (
     <>
       <CssBaseline />
-      <ResponsiveAppBar />
       <PageBody />
     </>
   );
