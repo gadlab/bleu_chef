032b72cbc21826b3b85b1456db52784cc9e0f66a
Gad <pygadina@gmail.com>
2021-12-13 12:12:50 +0100

    Simplified page structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import * as React from 'react';
-import PageBody from './components/page';
-
 import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import MainNavBar from './components/MainNavBar';
+import SignInSide from './components/SignInSide';
+import StickyFooter from './components/StickyFooter';
+
 import './App.css';
 
-export default function DisplayApp() {
-  return (
-    <>
-      <CssBaseline />
-      <PageBody />
-    </>
-  );
+const BCtheme = createTheme({
+    palette: {
+        primary: {
+            light: '#0d47a1',
+            main: '#5472d3',
+            dark: '#002171',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#b5ffff',
+            main: '#80d8ff',
+            dark: '#49a7cc',
+            contrastText: '#000',
+        },
+    },
+});
+
+const PageBody = () => {
+    return (
+      <>
+        <CssBaseline />
+        <ThemeProvider theme={BCtheme}>
+          <Container component="main" maxWidth="xs">
+            <MainNavBar />
+            <SignInSide />
+            <StickyFooter />
+          </Container>
+        </ThemeProvider>
+      </>
+    )
 }
+
+export default PageBody
